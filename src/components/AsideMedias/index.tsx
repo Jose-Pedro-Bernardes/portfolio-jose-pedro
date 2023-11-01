7dc45bb6b90015fb371a7497cce71e9ea0173170
jose-pedro <josepedro.bernardes1@gmail.com>
2023-10-31 23:17:27 -0300

    feat: add aside skeleton

--- src/components/AsideMedias/index.tsx
+++ src/components/AsideMedias/index.tsx
@@ -1,5 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-export default function index() {
-  return <div>index</div>
+export default function AsideMedias() {
+  return (
+    <aside>
+      <figure>
+        <Image
+          src=""
+          alt="Linha reta apontando para redes sociais logo abaixo."
+          width={10}
+          height={10}
+        ></Image>
+      </figure>
+      <nav>
+        <Link href="#">
+          <Image src="" alt="" width={10} height={10}></Image>
+        </Link>
+        <Link href="#">
+          <Image src="" alt="" width={10} height={10}></Image>
+        </Link>
+        <Link href="#">
+          <Image src="" alt="" width={10} height={10}></Image>
+        </Link>
+      </nav>
+      <section>
+        <button></button>
+      </section>
+    </aside>
+  )
 }
