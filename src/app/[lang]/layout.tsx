8baafc42003519edc670ebbdf1f820fe35efe912
jose-pedro <josepedro.bernardes1@gmail.com>
2023-11-18 20:02:42 -0300

    clear layout i18n config

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,15 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../../styles/globals.css'
 import '../../styles/styles.scss'
-import { i18n } from '@/config/i18n.config'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
-
-export async function generateStaticParams() {
-  const languages = i18n.locales.map((lang) => ({ lang }))
-  return languages
-}
 
 export const metadata: Metadata = {
   title: 'Portfolio - José Pedro Bernardes',
@@ -20,13 +14,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  params,
 }: {
   children: React.ReactNode
-  params: { lang: string }
 }) {
   return (
-    <html lang={params.lang}>
+    <html>
       <body className={inter.className}>
         <Header />
         {children}
