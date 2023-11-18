2eb66c9578f8d236851c3d58880957c7a6d66864
jose-pedro <josepedro.bernardes1@gmail.com>
2023-11-18 19:42:56 -0300

    remove: files to config i18n

--- src/dictionaries/default language collections/default en-US.ts
+++
@@ -1,7 +0,0 @@
-export const enUS = {
-  site: {
-    name: 'Portfolio - José Pedro Bernardes',
-    description:
-      'Full-Stack developer specialized in creating websites and managing them in a scalable and accessible manner.',
-  },
-}
