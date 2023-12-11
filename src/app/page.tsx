4773dd7a6c3f46d47fe9bac59bbc52c0609248e8
jose-pedro <josepedro.bernardes1@gmail.com>
2023-12-10 22:29:10 -0300

    small: parte do esqueleto da página principal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,12 @@
-import Image from 'next/image'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <>
-      <header>
-        <figure className="logo-container">
-          <Image
-            src="caminho/do/seu/logotipo.png"
-            alt="Logo da Empresa"
-            width="2"
-            height="2"
-          />
-          <figcaption>Texto do logotipo</figcaption>
-          <nav>
-            <a href="#" title="Página Inicial">
-              Início
-            </a>
-            <a href="#servicos" title="Nossos Serviços">
-              Serviços
-            </a>
-            <a href="#sobre" title="Sobre Nós">
-              Sobre
-            </a>
-            <a href="#portfolio" title="Nosso Portfólio">
-              Portfólio
-            </a>
-            <a href="#contato" title="Entre em Contato">
-              Contato
-            </a>
-          </nav>
-        </figure>
-      </header>
+      <Header />
       <main></main>
-      <footer></footer>
+      <Footer />
     </>
   )
 }
