f16a11de66a04ebec3a14007fccee4da0a995b49
jose-pedro <josepedro.bernardes1@gmail.com>
2023-12-10 22:17:13 -0300

    feat: novo component, Header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import Image from 'next/image'
+
 export default function Home() {
-  return <main>1234</main>
+  return (
+    <>
+      <header>
+        <figure className="logo-container">
+          <Image
+            src="caminho/do/seu/logotipo.png"
+            alt="Logo da Empresa"
+            width="2"
+            height="2"
+          />
+          <figcaption>Texto do logotipo</figcaption>
+          <nav>
+            <a href="#" title="Página Inicial">
+              Início
+            </a>
+            <a href="#servicos" title="Nossos Serviços">
+              Serviços
+            </a>
+            <a href="#sobre" title="Sobre Nós">
+              Sobre
+            </a>
+            <a href="#portfolio" title="Nosso Portfólio">
+              Portfólio
+            </a>
+            <a href="#contato" title="Entre em Contato">
+              Contato
+            </a>
+          </nav>
+        </figure>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  )
 }
