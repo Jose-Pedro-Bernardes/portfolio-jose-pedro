a17d1589a2dc7975c2c83c1f9d1746c62c622f76
jose-pedro <josepedro.bernardes1@gmail.com>
2023-12-10 22:16:47 -0300

    feat: esqueleto do menu de navegação.

---
+++ src/components/Header/MenuNav/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <nav>
+      <a href="#" title="Página Inicial">
+        Início
+      </a>
+      <a href="#servicos" title="Nossos Serviços">
+        Serviços
+      </a>
+      <a href="#sobre" title="Sobre Nós">
+        Sobre
+      </a>
+      <a href="#portfolio" title="Nosso Portfólio">
+        Portfólio
+      </a>
+      <a href="#contato" title="Entre em Contato">
+        Contato
+      </a>
+    </nav>
+  )
+}
