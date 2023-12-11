081c1ed3ca9a3ff090d8657b0efa65d33501bc25
jose-pedro <josepedro.bernardes1@gmail.com>
2023-12-10 21:47:58 -0300

    init: commit 0, redefinindo layout do projeto.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../../styles/globals.css'
 import '../../styles/styles.scss'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,11 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
--- src/user-info/user.ts
+++ src/user-info/user.ts
@@ -2,7 +2,4 @@ export const user = {
   firstName: 'José',
   name: 'José Pedro',
   email: 'josepedro.bernardes1@gmail.com',
-  copyright: '© Copyright 2023. Made by José',
-  thanks:
-    'Obrigado por visitar meu portfólio! Vamos conversar? Entre em contato!',
 }
