22e8e81d3e519e59d6cf4d2bbf48cb944653d18a
jose-pedro <josepedro.bernardes1@gmail.com>
2024-01-02 18:49:25 -0300

    feat: desktop header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,16 +6,18 @@ import './Header.styles.scss'
 export default function Header() {
   return (
     <header className="headerContainer">
-      <figure className="logoContainer">
-        <Image
-          src="/images/Logo.svg"
-          alt="Logo da Empresa"
-          width="33"
-          height="30"
-        />
-        <figcaption className="logoText">JPBM</figcaption>
-      </figure>
-      <MenuNav />
+      <div className="alignGrid">
+        <figure className="logoContainer">
+          <Image
+            src="/images/Logo.svg"
+            alt="Logo da Empresa"
+            width="33"
+            height="30"
+          />
+          <figcaption className="logoText">JPBM</figcaption>
+        </figure>
+        <MenuNav />
+      </div>
     </header>
   )
 }
