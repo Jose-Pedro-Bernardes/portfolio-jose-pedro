07f7ae58ee3e787eb7fb5ad381938c22399fdb81
jose-pedro <josepedro.bernardes1@gmail.com>
2024-01-03 20:26:36 -0300

    feat: services section

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -7,43 +7,46 @@ export default function Services() {
   return (
     <section id="servicos" className="services__container">
       <div className="alignGridx">
-        <header className="services__header">
-          <h3 className="services__h3">MEUS SERVIÇOS</h3>
-          <h2 className="services__h2">
-            Posso te <span className="colorBold">ajudar</span> com
-          </h2>
-          <span className="line"></span>
-        </header>
-        <article className="services__article">
-          <p className="article__p">
-            Minha experiência no WordPress, desenvolvo sites práticos que
-            simplificam a gestão de conteúdo. Ideal para aqueles que buscam um
-            parceiro comprometido e apaixonado por transformar ideias em
-            realidade. Estou pronto para colaborar de perto e ajudar a alcançar
-            seus objetivos online. Vamos trabalhar juntos para criar uma
-            presença digital eficaz e memorável!
-          </p>
-          <p className="article__p">
-            Além disso, sou especializado em front-end e proporciono soluções
-            tecnológicas avançadas com JavaScript, Typescript, React,
-            Styled-Components, Sass, Node.js e Express.js, garantindo aplicações
-            web de qualidade.
-          </p>
-          <div className="btn__container">
-            <button className="btnCall">Vamos conversar !</button>
-            <Link className="link__whatsapp" href={'#contato'}>
-              <Image
-                src="/images/links/Whatsapp.svg"
-                alt="Logo do WhatsApp"
-                width={30}
-                height={30}
-              />
-            </Link>
-          </div>
-        </article>
+        <div className="text__container">
+          <header className="services__header">
+            <h3 className="services__h3">MEUS SERVIÇOS</h3>
+            <h2 className="services__h2">
+              Posso te <span className="colorBold">ajudar</span> com
+            </h2>
+            <span className="line"></span>
+          </header>
+          <article className="services__article">
+            <p className="article__p">
+              Minha experiência no WordPress, desenvolvo sites práticos que
+              simplificam a gestão de conteúdo. Ideal para aqueles que buscam um
+              parceiro comprometido e apaixonado por transformar ideias em
+              realidade. Estou pronto para colaborar de perto e ajudar a
+              alcançar seus objetivos online. Vamos trabalhar juntos para criar
+              uma presença digital eficaz e memorável!
+            </p>
+            <p className="article__p">
+              Além disso, sou especializado em front-end e proporciono soluções
+              tecnológicas avançadas com JavaScript, Typescript, React,
+              Styled-Components, Sass, Node.js e Express.js, garantindo
+              aplicações web de qualidade.
+            </p>
+            <div className="btn__container">
+              <button className="btnCall">Vamos conversar !</button>
+              <Link className="link__whatsapp" href={'#contato'}>
+                <Image
+                  src="/images/links/Whatsapp.svg"
+                  alt="Logo do WhatsApp"
+                  width={30}
+                  height={30}
+                />
+              </Link>
+            </div>
+          </article>
+        </div>
         <aside className="services_content">
           <div className="services__box">
             <Image
+              className="boxImg"
               src="/images/BrandDesign.svg"
               alt="Ícone que sugere um design feito par uma marca."
               width={44}
@@ -53,6 +56,7 @@ export default function Services() {
           </div>
           <div className="services__box special box2">
             <Image
+              className="boxImg"
               src="/images/UIUX.svg"
               alt="Ícone que sugere design de UI e UX"
               width={44}
@@ -62,6 +66,7 @@ export default function Services() {
           </div>
           <div className="services__box special">
             <Image
+              className="boxImg"
               src="/images/wordpress.svg"
               alt="Ícone onde existe a logo do wordpress."
               width={44}
@@ -71,6 +76,7 @@ export default function Services() {
           </div>
           <div className="services__box box4">
             <Image
+              className="boxImg"
               src="/images/code.svg"
               alt="Ícone que sugere uma tag, que remete a programação."
               width={44}
